c3dc3019d0c5b57d1085db42a156489f93d0cc62
Richard Lindner <rlindner81@gmail.com>
2019-12-27 11:29:20 +0100

    Little better logs

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 "use strict"
 
+const { access } = require("fs").promises
 const { usage, parseArgs } = require("../src/args")
 const { queue } = require("../src/")
 
@@ -14,6 +15,12 @@ const { queue } = require("../src/")
 ;(async () => {
   const options = await parseArgs(process.argv.slice(2))
   if (options.help) {
+    return console.log(usage())
+  }
+  try {
+    await access(options.queueFile)
+  } catch (err) {
+    console.warn(`warning: could not access ${options.queueFile}`)
     return console.log(usage())
   }
   await queue(options)
