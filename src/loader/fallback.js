912d191d1745b8fe763789541ac9dd97b67c0ea8
Richard Lindner <rlindner81@gmail.com>
2019-12-27 19:35:29 +0100

    Keep the extension

--- src/loader/fallback.js
+++ src/loader/fallback.js
@@ -9,9 +9,7 @@ const { requestRaw } = require("../request")
 const finished = promisify(stream.finished)
 
 const load = async (url, urlParts) => {
-  const filename = urlParts.pathname
-    .replace(/^\//, "")
-    .replace(/\W/g, "-")
+  const filename = urlParts.pathname.replace(/^\//, "").replace(/[^.\w]/g, "-")
   console.log(`loading file ${filename}`)
   const response = await requestRaw({ url })
   const contentLength = parseFloat(response.headers["content-length"])
