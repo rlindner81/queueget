81dfce342eb802ec1ec015633a7d4b2d1399a0e5
Richard Lindner <rlindner81@gmail.com>
2019-12-27 11:29:38 +0100

    Less commented code

--- bin/cli.js
+++ bin/cli.js
@@ -4,14 +4,6 @@ const { access } = require("fs").promises
 const { usage, parseArgs } = require("../src/args")
 const { queue } = require("../src/")
 
-// process.on("exit", (...args) => {
-//   const i = 0
-// })
-
-// process.on("SIGINT", (...args) => {
-//   const i = 0
-// })
-
 ;(async () => {
   const options = await parseArgs(process.argv.slice(2))
   if (options.help) {
