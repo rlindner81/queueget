3d7a28607126d055e24f688f01032e26c48442cd
Richard Lindner <rlindner81@gmail.com>
2020-01-01 22:36:48 +0100

    Le Fix

--- src/loader/1fichier-com.js
+++ src/loader/1fichier-com.js
@@ -2,15 +2,33 @@
 
 const { commonload } = require("./common")
 const { request } = require("../request")
+const { sleep } = require("../helper")
+
+const _group = (regex, data) => {
+  const result = regex.exec(data)
+  if (result && result.length > 1) {
+    return result.slice(1)
+  }
+  return null
+}
 
 const load = async url => {
   const firstData = (await request({ url })).data
   const [, adz] = /name="adz" value="([.\d]+)"/.exec(firstData)
   const [, filename] = /Filename :<\/td>[\s\S]*?<td.*?>(.+?)<\/td>/.exec(firstData)
 
-  const secondData = (await request({ url, data: { adz } })).data
-  const [, realLink] = /href="(https:\/\/.+\.1fichier\.com\/.+)"/.exec(secondData)
-  await commonload({ filename, url: realLink })
+  for (;;) {
+    const secondData = (await request({ url, data: { adz } })).data
+    const waitTime = _group(/You must wait (\d+) minutes.../, secondData)
+    if (waitTime !== null) {
+      console.info(`sleeping for ${waitTime} minutes`)
+      await sleep(waitTime * 60)
+      continue
+    }
+    const realLink = _group(/href="(https:\/\/.+\.1fichier\.com\/.+)"/, secondData)
+    await commonload({ filename, url: realLink })
+    break
+  }
 }
 
 module.exports = {
