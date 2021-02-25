1beb3a817080ded5e434067e9a465f8897c8cf75
Richard Lindner <rlindner81@gmail.com>
2021-02-25 21:38:22 +0100

    almost working filefactory

---
+++ src/loader/www-filefactory-com.js
@@ -0,0 +1,49 @@
+"use strict"
+
+const { commonload } = require("./common")
+const { request, requestRaw } = require("../request")
+const { group, sleep } = require("../helper")
+
+const load = async (url, urlParts, queueStack, router) => {
+  const firstData = (await request({ url })).data
+  const link = group(
+    /<a class="wp-body-box-action-red" id="file-download-free-action-slow" href="#" data-href="(.\S+)">Slow Download<\/a>/,
+    firstData,
+    1
+  )
+  const filename = group(/^\S*\/(\S+)$/, link, 1)
+  console.log("loading %s", link)
+  let code = 0
+  while (true) {
+    let response = await requestRaw({ url: link })
+    if (response.statusCode === 200) {
+      break
+    } else if (response.statusCode === 302) {
+      const location = response.rawHeaders[response.rawHeaders.indexOf("Location") + 1]
+      code = parseFloat(group(/code=(\d+)/, location, 1))
+      if (code === 266) {
+        // short-wait
+        console.info("waiting on download %s", filename)
+        await sleep(60.5)
+      } else if (code === 275) {
+        // long-wait refresh-ip
+        if (router) {
+          console.info("limit exceeded refreshing ip")
+          await router.refreshIp()
+        }
+      } else {
+        throw new Error(`unexpected code ${code}`)
+      }
+      continue
+    } else {
+      throw new Error(`unexpected response status code ${response.statusCode}`)
+    }
+  }
+  console.log("after waiting")
+  return commonload({ filename, url: link })
+}
+
+module.exports = {
+  name: __filename.slice(__dirname.length + 1, -3),
+  load,
+}
