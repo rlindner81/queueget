d2bd2dd17e7d1b8fff40d5a664b323a7e0d1f966
Richard Lindner <rlindner81@gmail.com>
2021-05-11 09:03:03 +0200

    version 0.5.11 (#12)
    * added filefactory

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,6 +1,7 @@
 "use strict"
 
 module.exports = {
+  "www.filefactory.com": require("./www-filefactory-com"),
   "1fichier.com": require("./1fichier-com"),
   "mega.nz": require("./mega-nz"),
   fallback: require("./fallback"),
