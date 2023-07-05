9eb0db19e0cc7973ab55dd3ed241b93a5e519030
Richard Lindner <rlindner81@gmail.com>
2023-07-05 13:49:42 +0200

    fix test for usage log

--- test/args.test.js
+++ test/args.test.js
@@ -4,10 +4,10 @@ const { readFileSync } = require("fs");
 const { join } = require("path");
 const { usageText } = require("../src/args");
 
-test("usagelog", () => {
+test("usage log", () => {
   const readme = readFileSync(join(__dirname, "..", "README.md")).toString();
   const syntaxRe = /## Usage\n\n```([\w\W]*?)\n```/;
-  const usageDoc = syntaxRe.exec(readme)[1];
+  const usageDoc = syntaxRe.exec(readme)[1].replace(/usage: queueget/g, "usage: jest");
   const usageLog = usageText();
   expect(usageLog).toEqual(usageDoc);
 });
